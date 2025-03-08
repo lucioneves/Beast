e854fd9156416b97723cc0762f474374501d0b5c
Lucio Neves <luciocecilia20@gmail.com>
2025-03-08 16:40:56 -0400

    feat: funcionalidade de destruir o inimigo

---
+++ BeastFree/Assets/Shots/ShotController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
